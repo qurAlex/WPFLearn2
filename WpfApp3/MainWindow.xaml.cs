519ba14b86f5b8d005f3bf90f5bf91f7b03fabcb
Алексей Богданов <lexa.bogdanoc@gmail.com>
2022-06-21 20:46:09 +0300

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
